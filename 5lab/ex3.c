2faa53e560876a7367c6d0f90c177c0aa25ffc37
artyom-yurin <artem_yrin@mail.ru>
2018-09-20 00:00:21 +0300

    Update exerise 3

--- 5lab/ex3.c
+++ 5lab/ex3.c
@@ -1,33 +1,65 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 
 int count;
 int buffer[10];
+pthread_cond_t consumerCond;
+pthread_mutex_t count_mutex;
+pthread_cond_t producerCond;
+
+void producer();
 
 void consumer()
 {
-	while(count <= 0){}
-	count = count - 1;
-	printf("Pop: %d\n", buffer[count]);
-	sleep(60);
+	while(1)
+	{
+		while(count == 0)
+		{
+			pthread_cond_wait(&consumerCond, &count_mutex);
+		}
+		count = count - 1;
+		int item = buffer[count];
+		if(count == 9)
+		{
+			pthread_cond_signal(&producerCond);
+		}
+		printf("Item is %d\n", item);
+		sleep(60);
+	}
 }
 
-void producer(int i)
+void producer()
 {
-	while(count >= 10){}
-	buffer[count] = i % 5;
-	count = count + 1;
-	
+	while(1)
+	{
+		int item = 1;
+		while(count == 10)
+		{
+			pthread_cond_wait(&producerCond, &count_mutex);
+		}
+		buffer[count] = item;
+		count = count + 1;
+		if(count == 1)
+		{
+			pthread_cond_signal(&consumerCond);
+		}
+	}	
 }
 
 int main()
 {
-	count = 0;
-	while(1)
-	{
-		pthread_t thread[2];				
-		pthread_create(&thread[0], NULL, producer, (void*)(count));
-		pthread_create(&thread[1], NULL, consumer, NULL);
-	}
+	pthread_mutex_init(&count_mutex, NULL);
+	pthread_cond_init(&producerCond, NULL);
+	pthread_cond_init(&consumerCond, NULL);
+
+	count = 0; 
+	pthread_t thread[2];				
+	pthread_create(&thread[0], NULL, producer, NULL);
+	pthread_create(&thread[1], NULL, consumer, NULL);
+   	pthread_mutex_destroy(&count_mutex);
+	pthread_cond_destroy(&producerCond);
+	pthread_cond_destroy(&consumerCond);
+	pthread_exit(NULL);
 	return 0;
 }
