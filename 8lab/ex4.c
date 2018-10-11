4f06810abb429363db9e433caf4601fd5e06ddfb
artyom-yurin <artem_yrin@mail.ru>
2018-10-11 18:44:27 +0300

    Added forth exercise

---
+++ 8lab/ex4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/resource.h>
+
+int main()
+{
+  struct rusage r_usage;
+  for(int i = 0; i < 10; i++)
+  {
+    void* ptr = malloc(10 * 1024 * 1024);
+    memset(ptr, 0, 10 * 1024 * 1024);
+    getrusage(RUSAGE_SELF, &r_usage);
+    printf("Usage memory: %ld\n", r_usage.ru_maxrss);
+    sleep(1);
+    free(ptr);
+  }
+  return 0;
+}
